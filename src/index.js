9de1300697e19c69941871bca4be680149e5a312
SachokS <36642435+SachokS@users.noreply.github.com>
2018-02-22 01:46:48 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -28,13 +28,13 @@ return(this.array.length);
 sort(indices)
 {
 var count = indices[indices.length-1];
-var m = this.array;
+var k = this.array;
 for (var i = 0; i < count; i++)
 for (var j = indices[0]; j < count-i; j++)
-if (m[j]> m[j+1]) {
-var max = m[j];
-m[j] = m[j+1];
-m[j+1] = max;
+if (k[j]> k[j+1]) {
+var max = k[j];
+k[j] = k[j+1];
+k[j+1] = max;
 }
 
 }
